e651b64269c557f91d1c6801ff66c96087ffe618
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-16 18:19:09 +0100

    Get element js

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 console.log('JS OK')
 
+/*
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto 
+un messaggio in console con il numero della cella cliccata.
+MILESTONE 1
+Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
+MILESTONE 2
+Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. 
+Al click del bottone play, 
+vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+MILESTONE 3
+In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
+#MILESTONE 4
+Al click sulla cella, stampiamo il numero della cella cliccata in console, 
+poi coloriamo la cella d'azzurro!
+*/
+
+// Creo funzione per generare celle
+function createCell(number){
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.append(number);
+    return cell;
+}
+
+
+// Prendo elementi dal DOM
+const button = document.querySelector('.btn');
+const container = document.querySelector('.container');
+const h2 = document.querySelector('h2');
+const grid = document.querySelector('.grid');
+
